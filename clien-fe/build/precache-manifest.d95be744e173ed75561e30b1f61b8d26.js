5f9238e3205a964cce1deb124f2d5125b325857f
Dimitar80 <designcrafts.mk@gmail.com>
2020-02-23 21:10:51 +0100

    Add git folder

---
+++ clien-fe/build/precache-manifest.d95be744e173ed75561e30b1f61b8d26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e53dc00e5f0fb6d6d504e58488d1e52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdd95bf45e8165729b23",
+    "url": "/static/css/main.80c8318d.chunk.css"
+  },
+  {
+    "revision": "34692f6adada64309218",
+    "url": "/static/js/2.2a3a7ebc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.2a3a7ebc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd95bf45e8165729b23",
+    "url": "/static/js/main.c715f4a2.chunk.js"
+  },
+  {
+    "revision": "75923993b53a545bd74d",
+    "url": "/static/js/runtime-main.1b80de2e.js"
+  },
+  {
+    "revision": "579f77b47f0012b64615c1d15a2883bb",
+    "url": "/static/media/DPPHOTO.579f77b4.jpg"
+  }
+]);
