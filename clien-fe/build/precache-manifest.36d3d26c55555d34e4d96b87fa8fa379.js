d9b3292b6541446f64f2eacf8d83d0d4eb845579
Dimitar80 <designcrafts.mk@gmail.com>
2020-02-26 14:50:37 +0100

    26-02-2020-resmob

--- clien-fe/build/precache-manifest.36d3d26c55555d34e4d96b87fa8fa379.js
+++ clien-fe/build/precache-manifest.36d3d26c55555d34e4d96b87fa8fa379.js
@@ -1,20 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<< HEAD:clien-fe/build/precache-manifest.36d3d26c55555d34e4d96b87fa8fa379.js
     "revision": "1f94f18bc829eb99d7718751d24ce1eb",
     "url": "/index.html"
   },
   {
     "revision": "205cc240f0071cb6cd98",
     "url": "/static/css/main.65ac5334.chunk.css"
-=======
-    "revision": "57d3730c0a97dc083c5f80d40db13b0d",
-    "url": "/index.html"
-  },
-  {
-    "revision": "778c449b5cdf5e63d4c7",
-    "url": "/static/css/main.6bbc91bf.chunk.css"
->>>>>>> ef95f72bea711331466d38a09dfb194ad140e91c:clien-fe/build/precache-manifest.5a17b6dc8097e742fc4bd7f266852d58.js
   },
   {
     "revision": "34692f6adada64309218",
@@ -25,13 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.2a3a7ebc.chunk.js.LICENSE.txt"
   },
   {
-<<<<<<< HEAD:clien-fe/build/precache-manifest.36d3d26c55555d34e4d96b87fa8fa379.js
     "revision": "205cc240f0071cb6cd98",
     "url": "/static/js/main.a0f8e941.chunk.js"
-=======
-    "revision": "778c449b5cdf5e63d4c7",
-    "url": "/static/js/main.8b0ff4e3.chunk.js"
->>>>>>> ef95f72bea711331466d38a09dfb194ad140e91c:clien-fe/build/precache-manifest.5a17b6dc8097e742fc4bd7f266852d58.js
   },
   {
     "revision": "75923993b53a545bd74d",
